5002813eff1881aacdc83ced9915b8d48af3de1f
Jakub Drzymała <ordenix@otavi.pl>
2022-01-29 12:40:50 +0100

    final result

--- app/src/main/java/com/example/firstapp1/Activity2.kt
+++ app/src/main/java/com/example/firstapp1/Activity2.kt
@@ -1,14 +1,13 @@
 package com.example.firstapp1
 
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import org.json.JSONObject
 
 
 class Activity2 : AppCompatActivity() {
@@ -16,22 +15,24 @@ class Activity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
 
-        // Get message from last activity
         val postal_code = intent.getStringExtra(TEXT_TO_SEND)
 
-        // set mess
-        val textView = findViewById<TextView>(R.id.text_output).apply {
-            text = "Witgaj!! $postal_code"
-        }
+        val city = findViewById<TextView>(R.id.city)
+        val community = findViewById<TextView>(R.id.community)
+        val district = findViewById<TextView>(R.id.district)
+        val voivodeship = findViewById<TextView>(R.id.voivodeship)
 
         val queue = Volley.newRequestQueue(this)
         val url = "https://polish-zip-codes1.p.rapidapi.com/$postal_code"
 
         val req: JsonArrayRequest = object : JsonArrayRequest(Request.Method.GET,  url, null,
             Response.Listener {response ->
-                textView.text = "Motto na dziś ${response.getJSONObject(0).getString("miejscowosc")}"
+                city.text = response.getJSONObject(0).getString("miejscowosc")
+                community.text = response.getJSONObject(0).getString("gmina")
+                district.text = response.getJSONObject(0).getString("powiat")
+                voivodeship.text = response.getJSONObject(0).getString("wojewodztwo")
             }, Response.ErrorListener { error ->
-                textView.text = "ERROR: $error"
+                city.text = "ERROR: $error"
             }) {
             override fun getHeaders(): Map<String, String> {
                 val headers = HashMap<String, String>()
@@ -41,6 +42,9 @@ class Activity2 : AppCompatActivity() {
             }
         }
         queue.add(req)
+    }
+    fun back (view: View) {
+        finish()
     }
 }
 
