00af840f27dfe2ca1f3788b7daa51581921cb292
Jakub Drzymała <ordenix@otavi.pl>
2022-01-29 10:56:05 +0100

    test

--- app/src/main/java/com/example/firstapp1/Activity2.kt
+++ app/src/main/java/com/example/firstapp1/Activity2.kt
@@ -3,7 +3,12 @@ package com.example.firstapp1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +22,24 @@ class Activity2 : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.text_output).apply {
             text = "Witaj!! $message"
         }
+       // val textView = findViewById<TextView>(R.id.text)
+// ...
 
+// Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://kodpocztowy.intami.pl/api/41-500"
+// Request a string response from the provided URL.
+        val jsonObjectRequest = JsonArrayRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                textView.text = "Motto na dziś ${response.getJSONObject(0).getString("miasto")}"
+            },
+            Response.ErrorListener { error ->
+                // TODO: Handle error
+            }
+        )
+
+// Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
 
 
     }
