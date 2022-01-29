715b573f2088f021f435cab7e8c94d1b8b01f8fb
Jakub Drzymała <ordenix@otavi.pl>
2022-01-29 11:50:24 +0100

    test

--- app/src/main/java/com/example/firstapp1/Activity2.kt
+++ app/src/main/java/com/example/firstapp1/Activity2.kt
@@ -1,14 +1,15 @@
 package com.example.firstapp1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,31 +17,31 @@ class Activity2 : AppCompatActivity() {
         setContentView(R.layout.activity_2)
 
         // Get message from last activity
-        val message = intent.getStringExtra(TEXT_TO_SEND)
+        val postal_code = intent.getStringExtra(TEXT_TO_SEND)
 
         // set mess
         val textView = findViewById<TextView>(R.id.text_output).apply {
-            text = "Witaj!! $message"
+            text = "Witgaj!! $postal_code"
         }
-       // val textView = findViewById<TextView>(R.id.text)
-// ...
 
-// Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
-        val url = "http://kodpocztowy.intami.pl/api/41-500"
-// Request a string response from the provided URL.
-        val jsonObjectRequest = JsonArrayRequest(Request.Method.GET, url, null,
-            Response.Listener { response ->
-                textView.text = "Motto na dziś ${response.getJSONObject(0).getString("miasto")}"
-            },
-            Response.ErrorListener { error ->
-                // TODO: Handle error
+        val url = "https://polish-zip-codes1.p.rapidapi.com/$postal_code"
+
+        val req: JsonArrayRequest = object : JsonArrayRequest(Request.Method.GET,  url, null,
+            Response.Listener {response ->
+                textView.text = "Motto na dziś ${response.getJSONObject(0).getString("miejscowosc")}"
+            }, Response.ErrorListener { error ->
+                textView.text = "ERROR: $error"
+            }) {
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers["x-rapidapi-host"] = "polish-zip-codes1.p.rapidapi.com"
+                headers["x-rapidapi-key"] = "c12d626061msh43653fb2a1f88cbp1d0a56jsndebedd586ca5"
+                return headers
             }
-        )
-
-// Add the request to the RequestQueue.
-        queue.add(jsonObjectRequest)
-
-
+        }
+        queue.add(req)
     }
 }
+
+
